0fd08ffc6fe50370a95cd498ea6018e009dd78dd
Daniel Kan <xuechenkan@gmail.com>
2022-07-19 04:35:28 -0700

    add selfie image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,22 +2,27 @@ import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { LeftSection, Img, ImgContainer } from './HeroStyles';
 import { mePics } from '../../constants/constants';
+// import { Img } from '../Projects/ProjectsStyles';
 
 const Hero = (props) => (
   <section row nopadding>
     <LeftSection>
+      
+    {mePics.map((mePics) => (<Img src={mePics.image }/>))}
       <SectionTitle main center>
-        Hi, Welcome To <Img src={mePics.image }/> <br />
+        Hi, Welcome To <br />
         My Personal Portfolio
       </SectionTitle> 
+
       <SectionText>
         My Name is Xuechen (Daniel) Kan, I am currently UC San Diego undergrade under computer science major. An aspiring Web Developer that uses tools such as React JS and Next JS.
 
       </SectionText>
       <Button onClick ={()=> location.href = "#footer"}>Contact Me</Button>
     </LeftSection>
+
   </section>
 );
 
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
 
+export const Img = styled.img`
+  width:20%;
+  height:20%;
+  object-fit: cover;
+  overflow: hidden;
+  position: "absolute"; //Here is the trick
+  bottom: 0;
+  alignSelf: "flex-end";
+`
+
+export const ImgContainer = styled.section`
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+padding: 3rem;
+place-items: right;
+column-gap: 2rem;
+row-gap: 3rem;
+@media ${(props) => props.theme.breakpoints.sm} {
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  padding-bottom: 0;
+}
+
+`
+// export const RightSection = styled.div`
+//   width: 100%;
+//   @media ${(props) => props.theme.breakpoints.sm} {
+//     width: 80%;
+//     display: flex;
+//     flex-direction: column;
+
+//     margin: 0 auto;
+//   }
+//   @media ${(props) => props.theme.breakpoints.md} {
+//     width: 100%;
+//     display: flex;
+//     flex-direction: column;
+
+//     margin: 0 auto;
+//   }
+// `
 export const LeftSection = styled.div`
   width: 100%;
   @media ${(props) => props.theme.breakpoints.sm} {
