4afd618e38b0f47c08886ebcc04783292bd6f201
Daniel-Xkan <xuechenkan@gmail.com>
2025-04-09 16:33:08 -0700

    refactor Error component to use React.useEffect for redirection

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -8,11 +8,12 @@ const Error = () => {
                 Redirecting to new portfolio... If you are not redirected, click{' '}
                 <a href="https://daniel-xkan.github.io/my_portfolio/">here</a>.
             </p>
-            <script>
-                {`setTimeout(() => {
+            {React.useEffect(() => {
+                const timer = setTimeout(() => {
                     window.location.href = "https://daniel-xkan.github.io/my_portfolio/";
-                }, 3000);`}
-            </script>
+                }, 3000);
+                return () => clearTimeout(timer);
+            }, [])}
         </div>
     );
 };
